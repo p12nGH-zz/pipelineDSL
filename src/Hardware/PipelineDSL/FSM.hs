1f6337f3b1ca9993f78c24a46070098855eeea5b
Gleb Gagarin <gleb61@gmail.com>
2017-02-20 10:44:55 -0800

    FSM logic redesigned

--- src/Hardware/PipelineDSL/FSM.hs
+++ src/Hardware/PipelineDSL/FSM.hs
@@ -21,67 +21,51 @@ data FSMState a = FSMState
     , fsmStActivate :: Signal a
     , fsmStActive :: Signal a
     , fsmStPrevious :: [FSMState a]
-    , fsmStNext :: [FSMState a] }
+    , fsmStNext :: [(Signal a, FSMState a)] }
 
 type Signal a = HW.Signal a
 
 -- the FSM monad
-type FSMM a = RWST [FSMState a] [FSMState a] (FSMState a) (HW a)
+type FSMM a = RWST [(Int, Signal a)] [(Int, Signal a)] Int (HW a)
 
 or' = MultyOp Or
 and' = MultyOp And
 
+contextEnableSignal :: Int -> FSMM a (Signal a)
+contextEnableSignal i = do
+    enables <- ask
+    return $ or' $ map snd $ filter (\(x, _) -> x == i) enables
+
 fsm :: FSMM a b -> HW a b
 fsm f = fst <$> q where
-    q = mfix $ \ ~(_, states) -> do
-        let
-            hasNPrev n s = any ((==) n) $ map fsmStId $ fsmStPrevious s
-            nextTo n = filter (hasNPrev n) states
+    q = mfix $ \ ~(_, contexts) -> do
+        start <- mkRegI [(Lit 1 1, Lit 0 1)] $ Lit 1 1
+        let initial = tell [(0, start)]
+        (a, _, w) <- (runRWST (initial >> f)) contexts 0
 
-            -- find clear condition for initial state
-            initClr = or' $ map fsmStActivate $ nextTo 0
-
-        reg <- mkRegI [(initClr, Lit 0 1)] $ Lit 1 1
-        
-        let initialState = FSMState
-                { fsmStId = 0
-                , fsmStActivate = Lit 0 0
-                , fsmStActive = reg
-                , fsmStPrevious = []
-                , fsmStNext = [] }
-
-        (a, _, w) <- (runRWST f) states initialState
-        
         return (a, w)
 
--- record current state in Writer
--- create empty one in State
-wait :: Signal a -> FSMM a (FSMState a)
+wait :: Signal a -> FSMM a Int
 wait s = do
-    current_state <- get
-    states <- ask
+    current_context <- get
+    contexts <- ask
 
-    let
-        nextId = 1 + (fsmStId current_state)
-        hasNPrev s = elem nextId $ map fsmStId $ fsmStPrevious s
-        nextToMe = filter hasNPrev states
-        meclr = or' $ map fsmStActivate $ nextToMe
+    current_enable <- contextEnableSignal current_context
 
-    meset <- lift $ sig $ and' [s, fsmStActive current_state]
-    reg <- lift $ mkRegI [(meset, Lit 1 1), (meclr, Lit 0 1)] $ Lit 0 1
+    -- create state reg and next context enable logic
+    (_, next_enable) <- mfix $ \ ~(r, next) -> do
+        r' <- lift $ mkRegI [(current_enable, Lit 1 1), (next, Lit 0 1)] $ Lit 0 1
+        next' <- lift $ sig $ and' [r, s]
+        return (r', next')
 
-    let next = FSMState
-            { fsmStId = 1 + (fsmStId current_state)
-            , fsmStActivate = meset
-            , fsmStActive = reg
-            , fsmStPrevious = [current_state]
-            , fsmStNext = nextToMe }
+    let next = current_context + 1
     put next
-    tell [next]
+    tell [(next, next_enable)]
     return next
 
 infixl 2 .=
 (.=) r v = do
-    current_state <- get
-    lift $ addC r [(fsmStActivate current_state, v)]
+    current_context <- get
+    current_enable <- contextEnableSignal current_context
+    lift $ addC r [(current_enable, v)]
     return ()
